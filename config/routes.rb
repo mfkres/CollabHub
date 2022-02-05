a15087c0650b9538795329af49b1d587186676cb
Kristian Golemi <krigol0014@gmail.com>
2022-02-05 20:07:55 +0200

    Commenting

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  # /login redirects user to the devise/sessions/new.html.erb file which is the login page
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end

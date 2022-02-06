173cded3a0cf4075e5fc3cd8944871796affe893
Kristian Golemi <krigol0014@gmail.com>
2022-02-06 16:14:48 +0200

    Created and configured category and post models, added associations between them and the users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy  # when a user is deleted, delete all his posts
 end
